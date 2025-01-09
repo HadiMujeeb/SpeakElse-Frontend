a3303b50ad76baad48c4f5cd961a4cfb936dbad2
Hadi <hadimujeeb300@gmail.com>
2025-01-09 23:52:55 +0530

    update

--- src/environment/environment.development.ts
+++ src/environment/environment.development.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
     BACKEND_DOMAIN: 'http://localhost:3000',
-    production: false,
+    // production: false,
 };
 export const platFormId='63a6f2b87b7c230a89a0d9c4';
 
